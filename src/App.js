0be4e575d54eca6e4ed01502b83ea6061c15122b
github username <github email address>
2019-10-16 11:49:50 -0700

    added minScore(), maxScore, and tie(), as well as gameLoop which will help the AI move through the game.

--- src/App.js
+++ src/App.js
@@ -3,24 +3,30 @@ import './App.css';
 import Status from "./Status";
 import NewGame from "./NewGame";
 import Square from "./Square";
+// import { thisTypeAnnotation } from '@babel/types';
 
 class App extends Component {
-
   state = {
-    board: [
-      " ", " ", " ",
-      " ", " ", " ",
-      " ", " ", " "
-    ],
+    board: [" ", " ", " ", " ", " ", " ", " ", " ", " "],
     turn: "X",
     winner: null,
     maxPlayer: "X",
     minPlayer: "O"
-  }
+  };
+
+  clearBoard = () => {
+    this.setState({
+      board: [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+      turn: "X",
+      winner: null,
+      maxPlayer: "X",
+      minPlayer: "O"
+    });
+  };
 
   winner = (board, player) => {
     if(
-      (board[0] === player && board[1] === player && board[2] === player) || 
+      (board[0] === player && board[1] === player && board[2] === player) ||
       (board[3] === player && board[4] === player && board[5] === player) ||
       (board[6] === player && board[7] === player && board[8] === player) ||
       (board[0] === player && board[3] === player && board[6] === player) ||
@@ -28,16 +34,25 @@ class App extends Component {
       (board[2] === player && board[5] === player && board[8] === player) ||
       (board[0] === player && board[4] === player && board[8] === player) ||
       (board[2] === player && board[4] === player && board[6] === player)
-     ) {
-        return true;
-      } else {
-        return null;
-      }
+    ) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
-  copyBoard = (board) => {
+  tie = board => {
+    let moves = board.join("").replace(/ /g, "");
+    if (moves.length === 9) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  copyBoard = board => {
     return board.slice(0);
-  }
+  };
 
   validMove = (move, player, board) => {
     //checks if move is valid and if it is places move on board
@@ -48,23 +63,23 @@ class App extends Component {
     } else {
       return null;
     }
-  }
+  };
 
-  findMove = (board) => {
+  findMove = board => {
     let bestMoveScore = 100;
-    let move = null; 
+    let move = null;
     // test all possible moves if game is not over
-    if(this.winner(board, "X") || this.winner(board, "O") || this.tie(board)) {
+    if (this.winner(board, "X") || this.winner(board, "O") || this.tie(board)) {
       return null;
     }
 
     //iterates through the whole board and finds all valid moves
     //saves scores that are better than the bestMoveScore
-    for(let i =0; i < board.length; i++) {
+    for (let i = 0; i < board.length; i++) {
       //check every possible AI move and check if valid
       // if yes, return the board and call the function again on the board if it's vaild
       let newBoard = this.validMove(i, this.state.minPlayer, board);
-      if(newBoard) {
+      if (newBoard) {
         //assign a move score which needs to be the maximum of all positions
 
         let moveScore = this.maxScore(newBoard);
@@ -75,7 +90,98 @@ class App extends Component {
       }
     }
     return move;
-  }
+  };
+
+  minScore = board => {
+    if (this.winner(board, "X")) {
+      return 10;
+    } else if (this.winner(board, "O")) {
+      return -10;
+    } else if (this.tie(board)) {
+      return 0;
+    } else {
+      let bestMoveValue = 100;
+      for (let i = 0; i < board.length; i++) {
+        let newBoard = this.validMove(i, this.state.minPlayer, board);
+        if (newBoard) {
+          let predictedMoveValue = this.maxScore(newBoard);
+          if (predictedMoveValue < bestMoveValue) {
+            bestMoveValue = predictedMoveValue;
+          }
+        }
+      }
+      return bestMoveValue;
+    }
+  };
+
+  maxScore = board => {
+    if (this.winner(board, "X")) {
+      return 10;
+    } else if (this.winner(board, "O")) {
+      return -10;
+    } else if (this.tie(board)) {
+      return 0;
+    } else {
+      let bestMoveValue = -100;
+      for (let i = 0; i < board.length; i++) {
+        let newBoard = this.validMove(i, this.state.minPlayer, board);
+        if (newBoard) {
+          let predictedMoveValue = this.minScore(newBoard);
+          if (predictedMoveValue > bestMoveValue) {
+            bestMoveValue = predictedMoveValue;
+          }
+        }
+      }
+      return bestMoveValue;
+    }
+  };
+
+  gameLoop = move => {
+    let player = this.state.turn;
+    let currentBoard = this.validMove(move, player, this.state.board);
+    if (this.winner(currentBoard, player)) {
+      this.setState({
+        board: currentBoard,
+        winner: player
+      });
+      return;
+    }
+
+    if(this.tie(currentBoard)) {
+      this.setState({
+        board: currentBoard,
+        winner: "Tie"
+      });
+      return
+    }
+
+    player = "O";
+    currentBoard = this.validMove(
+      this.findMove(currentBoard),
+      player,
+      currentBoard
+    );
+
+    if (this.winner(currentBoard, player)) {
+      this.setState({
+        board: currentBoard,
+        winner: player
+      });
+      return;
+    }
+
+    if (this.tie(currentBoard)) {
+      this.setState({
+        board: currentBoard,
+        winner: "Tie"
+      });
+      return;
+    }
+
+    this.setState({
+      board: currentBoard
+    });
+  };
 
   render() {
     return (
@@ -83,15 +189,21 @@ class App extends Component {
         <div className="menu">
           <h1>Tic-Tac-Toe</h1>
           <Status winner={this.state.winner} />
-          <NewGame clearBoard={this.clearBoard}/>
+          <NewGame clearBoard={this.clearBoard} />
         </div>
-        {this.state.board.map((value, index )=> {
-           return <Square key={index} location={index} value={value} updateBoard={this.updateBoard} turn={this.state.turn} />
+        {this.state.board.map((value, index) => {
+          return (
+            <Square
+              key={index}
+              location={index}
+              value={value}
+              gameLoop={this.gameLoop}
+            />
+          );
         })}
       </div>
     );
   }
-  
 }
 
 export default App;
